b1a26ab19e18cfcf6c95bfecc9de1711d965bf1b
AfterBreeze <83800525+AfterBreeze@users.noreply.github.com>
2022-03-18 10:12:57 +0800

    Create 1-矩阵和.cpp

---
+++ 1-矩阵和.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e3 + 10;
+int s[N][N];
+int n, m, k;
+
+int main()
+{
+    scanf("%d%d%d", &n, &m, &k);
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+        {
+
+            scanf("%d", &s[i][j]);
+            s[i][j] += s[i][j - 1] + s[i - 1][j] - s[i - 1][j - 1];
+        }
+
+    while (k--)
+    {
+        int x1, x2, y1, y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        printf("%d\n", s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1]);
+    }
+
+    return 0;
+}
